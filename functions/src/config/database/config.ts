f310cccac22b315e95e6d8f3df013f3fb893e340
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2023-08-17 23:38:50 -0500

    json test

--- functions/src/config/database/config.ts
+++ functions/src/config/database/config.ts
@@ -1,16 +1,12 @@
 import * as admin from 'firebase-admin';
 // generate this file from your firebase project and put it in this folder
 require('dotenv').config();
+import * as serviceAccount from './permissions.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FB_PROJECT_ID,
-    clientEmail: process.env.FB_CLIENT_EMAIL,
-    privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  }),
-  databaseURL: process.env.FB_DATABASE_URL, 
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: process.env.FB_DATABASE_URL,
   storageBucket: process.env.FB_STORAGE_BUCKET,
-
 });
 
 export const db = admin.firestore();
